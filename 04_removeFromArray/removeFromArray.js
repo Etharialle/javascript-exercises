6818aaaca9ea90be6649da71a91c7bbe87eca233
etharialle <christopher.m.adams2@gmail.com>
2024-04-30 10:35:54 -0700

    removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removedValues) {
+    let result = array;
+    for (const value of removedValues) {
+        result = result.filter((arrayValue) => arrayValue !== value);
+    }
+    return result;
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 3, 2))
 // Do not edit below this line
 module.exports = removeFromArray;
